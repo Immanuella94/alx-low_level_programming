bc2c0942e893bd25c9cd18a4994321727c99f7a5
Immanuella94 <immanuella@gmail.com>
2021-06-17 13:07:27 -0700

    Updated file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,17 @@
+/*this is a print f example code*/
 #include <stdio.h>
 /**
- * this is a printf function
+ * this is where the main function starts
  */
 int main(void)
 {
+	/**
+	 * this is the function
+	 */
 	printf("with proper grammar, but the outcome is a piece of art,");
 
+	/**
+	 * this ends the main function
+	 */
 	return (0);
 }
