8c210852ba062577405eb74389eff3cdce91a975
Immanuella94 <immanuella@gmail.com>
2021-06-17 14:40:35 -0700

    Updated files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,11 @@
 
 #include <stdio.h>
 /*start of main program*/
+
 int main(void)
+	/* program does not expect any arguemnts
+	 * from start
+	 */
 {
 	/*puts function*/
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,17 @@
 /**
  * main - prints string
- * Description - prints \"with proper grammer, but the outcome is a piece of art,
+ * Description - prints 
+ * \"with proper grammer, but the outcome is a piece of art,
  * Return : 0
  */
 
 #include <stdio.h>
 /* this is where the main function starts */
+
 int main(void)
+	/* program does not expect any arguement
+	 * from start
+	 */
 {
 	/* this is the function */
 	printf("with proper grammar, but the outcome is a piece of art,");
