1811b265743000d1ee81093898ea7774cd84440f
Immanuella94 <immanuella@gmail.com>
2021-06-18 09:43:15 -0700

    Created 5-prints_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+/**
+ * main - Entry point
+ * prints base ten numbers
+ * Return: Always 0 (success)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	/*initialize int*/
+	int i = 0;
+
+	/*prints 0 through 9*/
+	for (i = 0; i < 9; i++)
+		printf("%d", i);
+	printf("\n");
+
+	/*end of main function*/
+	return (0);
+}
