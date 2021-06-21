276ed3e3c07815ae09b629425f5e5164feb95c1b
Immanuella94 <immanuella@gmail.com>
2021-06-21 04:10:16 -0700

    Updated file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,7 @@
 /**
  * main - Entry point
  * prints single digit number combinations
+ * seperaed by comma and space
  * Return: Always 0 (success)
  */
 
@@ -11,12 +12,18 @@ int main(void)
 	/*initialize int*/
 	int a;
 
-	/*code*/
-	for (a = '0'; a <= '9'; a++)
-		putchar(a);
-	if (a != '9')
-		putchar(',');
-	putchar(' ');
+	/*prints 1 to 9*/
+	for (a = 0; a <= 9; a++)
+	{
+		putchar('0' + a);
+		/*prints comma and space between numbers*/
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 
 	/*terminate main function*/
 	return (0);
