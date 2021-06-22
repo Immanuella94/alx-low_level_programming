8797742838e243fdb6f03c3aed9d213e82c558ad
Immanuella94 <immanuella@gmail.com>
2021-06-22 09:54:48 -0700

    Created 0-holbrton.c

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,26 @@
+/**
+ * main - Entry point
+ * description - prints Holberton
+ * and a new line
+ * Return: Always 0 (success)
+ */
+
+#include <stdio.h>
+#include "holberton.h"
+
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+
+	putchar('\n');
+
+	return (0);
+}
