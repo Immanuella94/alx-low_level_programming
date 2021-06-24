003991fec365f6bef668fec463ed288a0f187ebb
Immanuella94 <immanuella@gmail.com>
2021-06-24 12:47:46 -0700

    Created isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ *_isdigit - function that checks for digit 0 t0 9
+ *@c: character to be checked
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	while (c < 58)
+	{
+		if (c >= 48)
+		{
+			if (c <= 57)
+			{
+				return (1);
+			}
+		}
+		c++;
+	}
+
+	return (0);
+}
