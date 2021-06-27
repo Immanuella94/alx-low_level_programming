4b60fe41e2baf49876bbac3578be1f57f6ef3c9f
Immanuella94 <immanuella@gmail.com>
2021-06-27 12:32:14 -0700

    Ckes for lowercase characters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _islower - checks for lowercase character and new line
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 if lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c > 96)
+	{
+		if (c < 123)
+		{
+			return (1);
+		}
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+	return (0);
+}
