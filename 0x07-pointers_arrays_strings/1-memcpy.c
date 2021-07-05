cd46b3ca63e75200d935d2234387b3a2f9ea3ef0
Immanuella94 <immanuella@gmail.com>
2021-07-05 13:18:49 -0700

    Created 1-memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - copies memory area
+ *
+ * @dest: pointer to string to be copied to from @src
+ * @src: pointer to copie from to @dest
+ * @n: number of bytes to copy from @src to @dest
+ *
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
