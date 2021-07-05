c6854106214b7e2a2945fb25cdeeae277226c267
Immanuella94 <immanuella@gmail.com>
2021-07-05 13:39:56 -0700

    Created 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates a character in a string
+ *
+ * @s: string to search for charcter @c
+ * @c: find occurence of character
+ *
+ * Return: first occurence of character c in string s,
+ * or NULL if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s[i]);
+		}
+	}
+	return (0);
+}
