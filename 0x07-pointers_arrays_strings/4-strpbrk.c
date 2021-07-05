8006d258870e004340fec0c11755fc4ce7a9bf65
Immanuella94 <immanuella@gmail.com>
2021-07-05 14:40:09 -0700

    Created 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * _strpbk - locates the first occurence in @s of any of the bytes
+ * in @accept
+ *
+ * @s: pointer to string
+ * @accept: pointer to string
+ *
+ * Return: pointer to the byte in @s that matches one of the bytes in
+ * @accept, or NULL if no such byte is found
+ */
+
+char *_strpbk(char *s, char *accept)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		while (accept[i] != '\0')
+		{
+			if (s[i] == accept[i])
+			{
+				return (&s[i]);
+			}
+			i++;
+		}
+	}
+	return (0);
+}
