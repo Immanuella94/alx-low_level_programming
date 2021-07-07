92e19034951d757461a2dd568fb2148311442196
Immanuella94 <immanuella@gmail.com>
2021-07-07 08:56:56 -0700

    Created new directory, added files, created 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ *
+ * @s: pointer to string to be printed
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_pus_recursion(s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+_simple recursion codes_
