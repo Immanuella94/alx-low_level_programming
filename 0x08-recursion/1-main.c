1ddffceb7c7da83941b241b3f7dbef55ebcb7a4c
Immanuella94 <immanuella@gmail.com>
2021-07-07 11:04:36 -0700

    Created 1-print_rev_recursion

---
+++ 0x08-recursion/1-main.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for ALX School students
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_print_rev_recursion("Betty Holberton");
+	return (0);
+}
---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse followed by a new line
+ *
+ * @s: pointer to string to be printed
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar('\n');
+	}
+}
