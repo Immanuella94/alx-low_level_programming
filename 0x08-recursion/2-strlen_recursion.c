5b0370d0246b87947da33d52afe8b14e8c546fc1
Immanuella94 <immanuella@gmail.com>
2021-07-08 02:40:00 -0700

    Updated code

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -14,11 +14,10 @@ int _strlen_recursion(char *s)
 
 	len = 0;
 
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		len++;
-		len += _strlen_recursion(s + 1);
+		return (len);
 	}
-	_putchar('\n');
-	return (len);
+	len++;
+	return (len + _strlen_recursion(s + 1);
 }
