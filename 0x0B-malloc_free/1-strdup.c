09f7c71a9afefdf1fc8ef4919e0e173ad2ac7e2b
Immanuella94 <immanuella@gmail.com>
2021-07-13 11:49:35 -0700

    Added files

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - returns pointer with newlly allocated memory
+ * which has copy of @str
+ *
+ * @str: pointer to string
+ *
+ * Return: NULL if @str is NULL, pointer to duplictae string otherwise
+ */
+
+char *_strdup(char *str)
+{
+	char *str2;
+	char *ret = str2;
+
+	if (str == NULL)
+		return (NULL);
+
+	str2 = malloc(sizeof(*str2) * (strlen(str) + 1));
+
+	while (*str)
+	{
+		*str2++ = *str++;
+	}
+
+	if (!str2)
+	{
+		return (NULL);
+	}
+
+	return (ret);
+	free(str2);
+}
