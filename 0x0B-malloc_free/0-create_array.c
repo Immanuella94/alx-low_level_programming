66605dca0304360e4f91db105a6de864e2f57bad
Immanuella94 <immanuella@gmail.com>
2021-07-13 11:17:11 -0700

    Updated file

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,25 +13,23 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *ptr;
+	char *p;
 
 	if (size == 0)
-	{
 		return (NULL);
-	}
 
-	ptr = malloc(sizeof(*ptr) * size);
+	p = malloc(sizeof(*p) * size);
 
-	if (size != 0)
+	if (p)
 	{
 		while (i < size)
 		{
-			ptr[i] = c;
+			p[i] = c;
 			i++;
 		}
-		return (ptr);
+		return (p);
 	}
 	else
 		return (NULL);
-	free(ptr);
+	free(p);
 }
