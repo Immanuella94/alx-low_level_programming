523c9b02a4ba312b0309577f25bd4bf7065c9210
Immanuella94 <immanuella@gmail.com>
2021-07-14 01:58:11 -0700

    Corrected code

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,32 @@
 #include "holberton.h"
 #include <stdlib.h>
 
+/*
+ * is_number - checks for characters
+ *
+ * @s: string
+ *
+ * Return: 1 if number 0 otherwise
+ */
+int is_number(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) >= '0' && *(s + i) <= '9')
+		{
+			i++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
 /**
  * main - adds positive numbers
  *
@@ -13,27 +39,31 @@
 int main(int argc, char *argv[])
 {
 	int i;
-	int sum, n;
+	int sum;
+	int is_num;
 
+	sum = 0;
 	if (argc == 1)
 	{
 		printf("%d\n", 0);
-		return (0);
 	}
-	for (i = 1; i < argc; i++)
+	else if (argc > 1)
 	{
-		n = atoi(argv[i]);
-
-		if (n != 0)
+		i = 1;
+		for (i = 1; i < argc; i++)
 		{
-			sum += n;
+			is_num = is_number(argv[i]);
+			if (is_num == 1)
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		else
-		{
-			printf("Error\n");
-			return (0);
-		}
+		printf("%d\n", sum);
 	}
-	printf("%d\n", sum);
 	return (0);
 }
