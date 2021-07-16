7ccf06582b1d83c71ae1a3591fdfb9a5c90b47c9
Immanuella94 <immanuella@gmail.com>
2021-07-16 01:35:14 -0700

    Updated code

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,44 +2,52 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - concatenates two strings
+ * _strlen - return length of string
  *
- * @s1: string
- * @s2: string
- * @n: number of bytes to be copied from @s2
+ * @s: string to count
  *
- * Return: pointer to new string
+ * Return: the size
+ */
+
+int _strlen(char *s)
+{
+	int counter = 0;
+
+	while (*s != 0)
+	{
+		counter++;
+		s++;
+	}
+	return (counter);
+}
+
+/**
+ * *string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes
+ * Return: pointer to new space in memory that contains s1 + n bytes of s2
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int l1, l2, i, j;
-	char *s3;
+	unsigned int l1, l2, i, j;
+	char *p;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	l1 = l2 = 0;
-	while (s1[l1] != '\0')
-		l1++;
-	while (s2[l2] != '\0')
-		l2++;
+	l1 = s1 != NULL ? _strlen(s1) : 0;
+	l2 = s2 != NULL ? _strlen(s2) : 0;
+
 	if (n >= l2)
 		n = l2;
-	s3 = malloc((l1 + n + 1) * sizeof(char));
-	if (s3 == NULL)
+	p = malloc((l1 + n) * sizeof(char) + 1);
+	if (p == NULL)
 		return (NULL);
-	while (s1[i] != '\0')
+	for (i = 0; i < l1; i++)
 	{
-		s3[i] = s1[i];
-		i++;
+		p[i] = s1[i];
 	}
-	while (j < n && s2[j] != '\0')
-	{
-		s3[i] = s2[j];
-		i++, j++;
-	}
-	s3[i] = '\0';
-	return (s3);
+	for (j = 0; j < n && s2[j] != '\0'; j++, i++)
+		p[i] = s2[j];
+	p[i] = 0;
+	return (p);
 }
