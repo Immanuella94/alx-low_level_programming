ac7e3edeb9b1080c4f7f9c31dfc60edacaf4edc3
Immanuella94 <immanuella@gmail.com>
2021-07-19 14:07:03 -0700

    Added files

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -15,6 +16,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-
-	return (d);
 }
