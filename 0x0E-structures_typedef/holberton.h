574a8ca7ecc2d9c5a681751edbe0d52ee01952f7
Immanuella94 <immanuella@gmail.com>
2021-07-19 15:32:30 -0700

    Updated code

--- 0x0E-structures_typedef/holberton.h
+++
@@ -1,9 +0,0 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-#include <stdio.h>
-
-int _putchar(char c);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
-#endif /* HOLBERTON_H */
