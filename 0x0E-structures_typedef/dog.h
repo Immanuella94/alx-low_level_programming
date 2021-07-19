ed8830a321ec363ae992038155b51328f074ed15
Immanuella94 <immanuella@gmail.com>
2021-07-19 13:23:27 -0700

    Updated code

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,11 @@
+/**
+ * struct dog - dog details
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog's owner name
+ *
+ * Description: dog's details and owner's name
+ */
 struct dog
 {
 	char *name;
