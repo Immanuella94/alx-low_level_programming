58f24ca7dda4b54c12536fc962bd673f35b77426
Immanuella94 <immanuella@gmail.com>
2021-07-22 13:38:43 -0700

    Added files

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,23 +2,28 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - prints numbers followed by a new line
+ * print_strings - prints numbers followed by a new line
  * @separator: string to be printed between numbers
  * @n: number of integers passed to function
  *
  * Return: void
  */
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
+	char *str;
 	va_list ap;
 
 	do {
 		va_start(ap, n);
 		for (i = 1; i < n; i++)
 		{
-			printf("%d", va_arg(ap, int));
-			if (i != n && separator != NULL)
+			str = va_arg(ap, char *);
+			if (str == NULL)
+				printf("(nil)");
+			else
+				printf("%s", str);
+			if (i != NULL && separator != NULL)
 				printf("%s", separator);
 		}
 		va_end(ap);
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -2,3 +2,4 @@ _Variadic Functions:_
 _Tasks:_
 0. Beauty is variable, ugliness is constant
 1. To be is to be the value of a variable
+2. One womans constant is another womans variable
