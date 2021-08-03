ddc115050f4312766ca36da43a22837ed2826cca
Immanuella94 <immanuella@gmail.com>
2021-08-03 08:49:34 -0700

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - freea a listint_t list
+ * @head: pointer to first node
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	if (*head != NULL)
+		(*head)->next = NULL;
+	free(*head);
+}
